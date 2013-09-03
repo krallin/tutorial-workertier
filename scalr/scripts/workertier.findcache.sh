46badfbcb11467638becabb8342bf5f9e2e556d4
Thomas Orozco <thomas@orozco.fr>
2013-09-03 14:01:56 +0200

    Update Scalr scripts

---
+++ scalr/scripts/workertier.findcache.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# NOTE: This script MUST RUN as async
+# NOTE: This script should run LAST in your HostUp chain
+set -o nounset
+set -o errexit
+
+sleep 5 # Huge hack to give IpListBuilder time to run
+
+PIDFILE=/var/run/workertier.$WORKERTIER_ROLE.pid
+kill -s USR1 `cat $PIDFILE`
