ab579cbe9d68adee26209a8b6b092ef9895fda65
Thomas Orozco <thomas@orozco.fr>
2013-09-03 21:15:48 +0200

    Fix Invalid GV used in rabbit configure

--- scalr/scripts/rabbit.configure.sh
+++ scalr/scripts/rabbit.configure.sh
@@ -5,4 +5,4 @@ set -o nounset
 # We will accept a fail on all those commands.
 rabbitmqctl add_vhost $RABBIT_VHOST  || true  # We don't care if this already exists
 rabbitmqctl add_user $RABBIT_USERNAME $RABBIT_PASSWORD || rabbitmqctl change_password $RABBIT_USERNAME $RABBIT_PASSWORD
-rabbitmqctl set_permissions -p $RABBIT_VHOST $RABBIT_USER ".*" ".*" ".*"
+rabbitmqctl set_permissions -p $RABBIT_VHOST $RABBIT_USERNAME ".*" ".*" ".*"
