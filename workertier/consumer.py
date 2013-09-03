60db080f06987122edc66e73e056f3a6b6102ae8
Thomas Orozco <thomas@orozco.fr>
2013-09-03 14:23:15 +0200

    Ensure the key is used as a string

--- workertier/consumer.py
+++ workertier/consumer.py
@@ -14,7 +14,7 @@ class Consumer(object):
         self.num_consumers = num_consumers
 
     def consume_message(self, message):  #TODO: Create a thicker layer on messages
-        key = message.body
+        key = str(message.body)
         value = key.swapcase()
         self.cache.set(key, value)
         logger.debug("Processed message: '%s' -> '%s'", key, value)
